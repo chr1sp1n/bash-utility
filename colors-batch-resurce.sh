d6db2ccfb9da18af48317e9ec271bd77a3b01f49
Chris <chr1sp1n.dev@gmail.com>
2017-04-20 11:41:20 +0200

    Bash colors shortcut

---
+++ colors-batch-resurce.sh
@@ -0,0 +1,44 @@
+#Forecolor Regular
+txtblk='\e[0;30m' 		# Nero
+txtred='\e[0;31m' 		# Rosso
+txtgrn='\e[0;32m' 		# Verde
+txtylw='\e[0;33m' 		# Giallo
+txtblu='\e[0;34m' 		# Blu
+txtorg='\033[38;5;208m' # Orange
+txtpur='\e[0;35m' 		# Viola
+txtcyn='\e[0;36m' 		# Ciano
+txtwht='\e[0;37m' 		# Bianco
+
+#Forecolor Bold
+bldblk='\e[1;30m' # Nero - Bold
+bldred='\e[1;31m' # Rosso
+bldgrn='\e[1;32m' # Verde
+bldylw='\e[1;33m' # Giallo
+bldblu='\e[1;34m' # Blu
+bldpur='\e[1;35m' # Viola
+bldcyn='\e[1;36m' # Ciano
+bldwht='\e[1;37m' # Bianco
+
+#Underline
+unkblk='\e[4;30m' # Nero
+undred='\e[4;31m' # Rosso
+undgrn='\e[4;32m' # Verde
+undylw='\e[4;33m' # Giallo
+undblu='\e[4;34m' # Blu
+undpur='\e[4;35m' # Viola
+undcyn='\e[4;36m' # Ciano
+undwht='\e[4;37m' # Bianco
+
+#Background
+bakblk='\e[40m'   # Nero
+bakred='\e[41m'   # Rosso
+bakgrn='\e[42m'   # Verde
+bakylw='\e[43m'   # Giallo
+bakblu='\e[44m'   # Blu
+bakpur='\e[45m'   # Viola
+bakcyn='\e[46m'   # Ciano
+bakwht='\e[47m'   # Bianco
+bakorg='\033[48;5;208m' # Orange
+
+#Reset
+txtrst='\e[0m'    # Text Reset
